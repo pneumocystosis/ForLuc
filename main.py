eee110bb96139e490f79bfc38895e8379aa5ab34
pneumocystosis <69307109+pneumocystosis@users.noreply.github.com>
2020-10-12 14:13:53 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+nbApparitions=[0]*26
+texte=input().lower()
+compteLettres=0
+
+for lettre in range(len(texte)):
+    numLettre=ord(texte[lettre])-ord("a")
+    if numLettre>=0 and numLettre<=25:
+        nbApparitions[numLettre]+=1
+        compteLettres+=1
+        
+#affichage, pas utile dans ton programme
+for lettre in nbApparitions:
+    print(lettre/compteLettres)
